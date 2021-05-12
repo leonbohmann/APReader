99b65ff6a285a30fd0983518cdf3be465d06a9e4
Leon Bohmann <leon.bohmann@mepla.net>
2021-05-12 10:08:38 +0200

    update doc

--- apread/__init__.py
+++ apread/__init__.py
@@ -1,3 +1,15 @@
+"""
+CatmanAP procudes .bin files after each measurement. While it is possible to export as a different format (i.e. txt or asc) it's not efficient because one has to change the export format after every measurement. Here comes the treat: Just export as binary and use this package to work with binary files directly.
+
+After reading all channels from the binary file, the channels are analyzed and every measure-channel will receive a reference to a time channel, depending on the amount of entries in the channels and the fact, that the time-channel has to contain "time" or "zeit" in its name. What that means is, that a channel with x entries and the name "time - 1" will be regarded as the time-channel of any other channel with x Data Entries.
+
+
+Installation
+-------------------
+
+    pip install apread
+"""
+
 # the main apreader
 from apread.apreader import *
 # binary reader
--- build.bat
+++ build.bat
@@ -6,3 +6,8 @@ rmdir /Q /S dist
 echo Removing build
 rmdir /Q /S build
 python setup.py sdist bdist_wheel
+
+
+pdoc --html --output-dir docs apread --force
+xcopy /e /k /h /i /q /s /y .\docs\apread .\docs
+rmdir .\docs\apread /Q /S
