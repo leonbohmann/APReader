bc407a7f997d7b3cb42b5c87da0b0eed6ec4584f
Leon Bohmann <leon.bohmann@mepla.net>
2021-07-18 11:27:08 +0200

    updated version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.9.4",
     ],
     packages=['apread'],
     install_requires=['matplotlib', 'plotly', 'scipy', 'typing', 'tqdm', 'pandas'],
