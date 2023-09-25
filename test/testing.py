85574c71b6a9aae02c44c6dae3bcf73f1b570ba6
Leon Bohmann <leon.bohmann@mepla.net>
2023-09-25 15:33:17 +0200

    clean testing

--- test/testing.py
+++ test/testing.py
@@ -15,71 +15,28 @@ if __name__ == '__main__':
     # specify wether to use parallel loading of channel data
     loadInParallel = False
     speedTest = False
-    
+
     if loadInParallel:
-        pool = mp.Pool()        
-    
+        pool = mp.Pool()
+
     # t0 = datetime.now()
     # # create a reader
     # for i in range(1,1000):
     #     reader = APReader(file, parallelPool=pool)
 
     # t1 = datetime.now()
-    
+
     # print(t1-t0)
 
     t0 = datetime.now()
     # create a reader
-    for i in range(1,1000 if speedTest else 1):
+    for i in range(1,1000 if speedTest else 2):
         reader = APReader(file, parallelPool=pool)
 
     t1 = datetime.now()
-    
+
     print(t1-t0)
 
     if loadInParallel:
         pool.close()
         pool.join()
-    ## print all single channels
-    #for channel in reader.Channels:    
-    #    print (f"{channel.Name}: {len(channel.data)} Entries")
-
-    for group in reader.Groups:
-        print("--------------")
-        print (f"Group ({group.ChannelX.Name})")
-
-        for channel in group.Channels:
-            print (f"\t{channel.Name:20}: {len(channel.data)} Entries")        
-
-        print("--------------")
-        
-        
-    reader.plot()
-
-def channel_dates():
-    reader = APReader("file.bin")
-    
-    for channel in reader.Channels:
-        print(channel.date) # 12.1.2023: 18:08 ...
-
-def test2():
-    reader = APReader("file.bin")
-
-    for group in reader.Groups:
-        # alle channels in der gruppe
-        channels = group.Channels
-        # X-Channel der Gruppe (Zeit)
-        channelX = group.ChannelX
-        # alle Y channels, also Messreihen der Gruppe
-        channelsY = group.ChannelsY
-        
-        # data in z.B. der ersten Messreihe
-        channel1 = channelsY[0]
-        # das ist das numpy-array mit den tatsächlichen Werten drin
-        data = channel1.data
-        print(data)
-        
-    # ALTERNATIV (alle channels in der bin Datei):
-    for channel in reader.Channels:
-        print(channel.data)
-        
