802e8a28a5ffda4bc870c253cbca9b9b87c21ce5
Leon Bohmann <leon.bohmann@mepla.net>
2023-09-27 10:30:51 +0200

    update collection methods

--- apread/apreader.py
+++ apread/apreader.py
@@ -20,7 +20,7 @@ from apread.binaryReader import BinaryReader
 from apread.entries import Channel, Group
 
 def get_cmap(n, name='jet'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.get_cmap(name, n)
 
@@ -38,18 +38,18 @@ def plot_multiple_datasets(datasets: list[Tuple[nptyp.NDArray, nptyp.NDArray, st
     for i, data in enumerate(datasets):
         x, y, style, ylabel, title = data
 
-            
+
         if i == 0:
             ax1 = ax
         else:
             ax1 = ax.twinx()
             ax1.spines['right'].set_position(('outward', 60*(i-1)))
 
-        if style is None: 
+        if style is None:
             l = ax1.plot(x, y, color=cmap(i), label=title)[0]
         else:
             l = ax1.plot(x, y, style, label=title)[0]
-            
+
         ax1.set_ylabel(ylabel, color=l.get_color())
         ax1.tick_params(axis='y', colors=l.get_color())
         lbs.append((l, ylabel))
@@ -70,18 +70,18 @@ def align_yaxis(ax1, v1, ax2, v2):
     ax2.set_ylim(miny+dy, maxy+dy)
 
 def get_cmap(n, name='hsv'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
 
 class APReader:
-    
+
     """
-    All channels unsorted.    
+    All channels unsorted.
     """
     Channels: List[Channel]
     """
-    Grouped channels.    
+    Grouped channels.
     """
     Groups: List[Group]
 
@@ -101,16 +101,16 @@ class APReader:
         self.parallelPool = parallelPool
         self.Channels = []
         self.Groups = []
-        
+
         if self.parallelLoad:
             print(f'INFO: Using {len(mp.active_children())} processes to load data.')
-        
+
         self.read()
         self.connect()
 
     def connect(self):
         """
-        Find channels with equal data length and filter the name for "time" to 
+        Find channels with equal data length and filter the name for "time" to
         connect the time-channel with every value-channel (Channel -> channel.Time).
         """
         # no channels no connection
@@ -126,10 +126,10 @@ class APReader:
             if channel.length not in channelGroups:
                 # add a new dictionary entry
                 channelGroups[channel.length] = []
-            
+
             # append the channel to the dictionary's entry
             channelGroups[channel.length].append(channel)
-                
+
         # now, for each channel-group find the time-channel (only if length of the gorup > 0)
         for group in [channelGroups[groupLen] for groupLen in channelGroups if groupLen > 0]:
             # sometimes only one channel is in the group
@@ -138,10 +138,10 @@ class APReader:
                     chan.broken = True
                     self.Channels.remove(chan)
                 continue
-            
-            
+
+
             # find the time channel in the group
-            timeChannel = None            
+            timeChannel = None
             for channel in group:
                 # condition: channel name has to contain "Zeit"
                 if re.match(r"([T|t]ime)|([Z|z]eit)",channel.Name) is not None:
@@ -154,11 +154,11 @@ class APReader:
                     # this should be uncommented for the testing timing functions to be usable
                     # timeChannel = channel
                     # break
-                
+
                     if input(f"Is '{channel.Name}' your time/reference channel? [y/n] ") == "y":
                         timeChannel = channel
                         break
-                                
+
             # set the time-channel on every channel but itself
             if timeChannel != None:
                 for channel in group:
@@ -171,13 +171,13 @@ class APReader:
                 print("\t [ERROR] Channel-group does not contain a time-channel!")
                 print("\t  The current group will not be included in this output.")
                 continue
-            
+
             # create new group based on the groups listed
             self.Groups.append(Group(group, self.fileName, self.verbose))
-        
+
         if len(self.Channels) > 0:
             self.date = self.Channels[0].date
-        
+
         pass
 
     def __iter__(self):
@@ -196,7 +196,7 @@ class APReader:
         Read the binary file.
 
         Creates channels which can be later accessed.
-        """        
+        """
         # start by opening a binary stream on the filepath
         with open(self.filepath, 'rb') as f:
             # create a binary reader to simplify inputs
@@ -223,7 +223,7 @@ class APReader:
             # readaway
             for i in range(self.numChannels):
                 reader.read_int32()
-            
+
             # reduced factor (unused)
             _ = reader.read_int32()
 
@@ -234,7 +234,7 @@ class APReader:
                 channel = Channel(reader, self.fileName, self.filepath,\
                     self.verbose, self.parallelPool)
 
-                if not channel.broken and channel.length > 0:                    
+                if not channel.broken and channel.length > 0:
                     self.Channels.append(channel)
                 elif self.verbose:
                     print(f'\t[ {self.fileName} ] Skipping channel (zero length or invalid data)')
@@ -250,35 +250,78 @@ class APReader:
                 channel.readData()
 
             if self.verbose:
-                print(f'\t[ {self.fileName} ] Done. {len(self.Channels)} Channels left after filtering.') 
-       
-    def collectChannels(self, channel_names: list[str]) -> list[Channel] | Channel:
+                print(f'\t[ {self.fileName} ] Done. {len(self.Channels)} Channels left after filtering.')
+
+    # def collectChannelsFiltered(self, filter: str) -> list[Channel]:
+    #     chans = []
+
+
+    #     for c in self.Channels:
+    #         if re.match(filter, c.Name) is not None:
+    #             chans.append(c)
+    #     # for c in self.Channels:
+    #     #     if any([x in c.Name for x in channel_names]):
+    #     #         chans.append(c)
+
+    #     return chans if len(chans) > 1 else chans[0]
+    def transformWildcardFilter(filter: str) -> str:
+        """Transforms a wildcard filter to regex.
+
+        Args:
+            filter (str): Simplified wildcard filter.
+
+        Returns:
+            str: Regex filter equivalent.
+        """
+        filter = filter.replace("*",".*")
+        # filter = filter.replace("?",".+")  # deactivated, because it disables "?" support
+        return filter
+
+    def collectChannelsLike(self, filter: str) -> list[Channel]:
+        """Collect channels that match the filter.
+
+        Args:
+            filter (str): Simplified wildcard filter.
+
+        Returns:
+            list[Channel]: Matched channels.
+        """
+        filter = APReader.transformWildcardFilter(filter)
+
         chans = []
-        
+
+        for c in self.Channels:
+            if re.match(filter, c.Name) is not None:
+                chans.append(c)
+
+        assert len(chans) > 0, f'No channels found for filter "{filter}".'
+
+        return chans
+
+    def collectChannels(self, channel_names: list[str]) -> list[Channel]:
+        chans = []
+
         for cname in channel_names:
             for c in self.Channels:
-                if c.Name == cname:
+                if re.match(cname, c.Name) is not None:
                     chans.append(c)
-        # for c in self.Channels:
-        #     if any([x in c.Name for x in channel_names]):
-        #         chans.append(c)
-                
-        return chans if len(chans) > 1 else chans[0]
-      
+
+        return chans
+
     def collectDatasets(self, channel_names: list[str]) -> list[Tuple[nptyp.NDArray, nptyp.NDArray, str, str, str]]:
         chans = []
         for c in self.Channels:
             if any([x in c.Name for x in channel_names]):
                 chans.append((c.Time.data, c.data, None, f'{c.Name}[{c.unit}]', c.Name))
-                
+
         return chans
-              
+
     def plot(self, groupIndices=None, sameAxis = False):
         """Plots the complete file.
         """
         for group in self.Groups:
-            group.plot(sameAxis = sameAxis)     
-            
+            group.plot(sameAxis = sameAxis)
+
     def plotGroup(self, channelIndex):
         """Plot a specific channel
 
@@ -287,7 +330,7 @@ class APReader:
         """
         for group in self.Groups:
             group.plotChannels(channelIndex, channelIndex)
-    
+
     def plotGroups(self, start, end):
         """Plot a range of channels
 
@@ -296,9 +339,9 @@ class APReader:
             end (int): Ending index, supports -[index] to mark index from the end.
         """
         for group in self.Groups:
-            group.plot(range(start,end))                         
-            
-            
+            group.plot(range(start,end))
+
+
     def printSummary(self):
         for group in self.Groups:
             print('---------')
